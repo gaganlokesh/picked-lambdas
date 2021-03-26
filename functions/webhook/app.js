9b372a7653488c3d7fa2c0f47ae28524a0ae11f3
Gagan Lokesh <gaganlokesh@gmail.com>
2021-03-26 21:09:38 +0530

    INITIAL COMMIT 🎉
    - Initialize AWS SAM app for processing feed content
    
    - Define Step Function state machine and add API Gateway as trigger
    
    - Add dummy webhook lambda function

---
+++ functions/webhook/app.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Object} event - Input event to the Lambda function
+ * @param {Object} context - Lambda Context runtime methods and attributes
+ * 
+ */
+exports.webhook = async (event, context) => {
+    console.log(event.body);
+    response = {
+        'statusCode': 200,
+        'body': JSON.stringify({
+            message: 'hello world'
+        })
+    }
+
+    return response;
+};
