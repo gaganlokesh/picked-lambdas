715508845313e527314b72ba6a462c15278dab31
Gagan Lokesh <gaganlokesh@gmail.com>
2021-03-30 16:08:26 +0530

    Add functionality to parse webhook request and retrieve feed items
    - Verify request signature before continuing with request body parsing
    
    - Handle error cases with a catch block in state-machine definition

--- functions/webhook/app.js
+++ functions/webhook/app.js
@@ -1,15 +1,61 @@
+const crypto = require("crypto");
+
+class UnrecognizedRequestError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
+const epochToDateTime = (epoch) => epoch ? new Date(epoch * 1000) : null;
+
+const parseFeedItems = (items) => {
+  let feedItems = [];
+
+  if (items.length > 0) {
+    feedItems = items.map((item) => ({
+      id: crypto.createHash('md5').update(item.id).digest('hex'),
+      title: item.title,
+      url: item.permalinkUrl,
+      publishedAt: epochToDateTime(item.published),
+      updatedAt: epochToDateTime(item.updated),
+      tags: item.categories ? item.categories.map(c => c.toLowerCase()) : [],
+    }))
+  }
+
+  return feedItems;
+}
+
 /**
+ * Lambda function to process webhook requests from superfeedr.
+ * This function parses the request body and creates a list of feed items.
+ * 
  * @param {Object} event - Input event to the Lambda function
  * @param {Object} context - Lambda Context runtime methods and attributes
- * 
  */
 exports.webhook = async (event, context) => {
-  console.log(event.body);
-  response = {
-    'statusCode': 200,
-    'body': JSON.stringify({
-      message: 'hello world'
-    })
+  const { body, headers } = event;
+
+  if (!body || Object.keys(body).length === 0) {
+    console.warn("Empty body provided in request");
+    throw new UnrecognizedRequestError("Request body cannot be empty");
+  }
+
+  const hmac = crypto.createHmac('sha1', process.env.WebhookSecret);
+  const signature = `sha1=${hmac.update(JSON.stringify(body)).digest('hex')}`;
+  if (headers['x-hub-signature'] !== signature) {
+    console.warn('Failed to verify hub signature');
+    throw new UnrecognizedRequestError("Failed to verify hub signature");
+  }
+
+  if (body.status?.code !== 200 || body.items?.length === 0) {
+    // Stop execution
+    console.warn("No feed items to process");
+    throw new UnrecognizedRequestError("No feed items to process");
+  }
+
+  const response = {
+    items: parseFeedItems(body.items || [])
   }
 
   return response;
