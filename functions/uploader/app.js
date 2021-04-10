8247fb79a2898ed50881e95b83a15ee8813c0faa
Gagan Lokesh <gaganlokesh@gmail.com>
2021-04-11 01:00:56 +0530

    Add new lambda function to upload article image to S3
    - Update SAM template to include new uploader function and S3 bucket
    
    - Update state-machine to add S3 upload as a step after metadata scraping

---
+++ functions/uploader/app.js
@@ -0,0 +1,82 @@
+const crypto = require('crypto');
+const got = require('got');
+const sharp = require('sharp');
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+
+const getImage = async (url) => {
+  return got(url, { responseType: 'buffer' })
+    .then(res => {
+      return res.body;
+    })
+    .catch(err => {
+      console.warn("Error downloading image:", err);
+      throw err;
+    })
+  ;
+}
+
+const uploadImage = async (buffer, metadata) => {
+  const keyPrefix = process.env.IMAGE_PREFIX;
+  const keyName = crypto.createHash('md5')
+    .update(buffer, 'utf8')
+    .digest('hex');
+  const key = `${keyPrefix}${keyName}.${metadata.format}`
+  let objectParams = {
+    Bucket: process.env.IMAGE_BUCKET,
+    Key: key,
+    ACL: 'public-read',
+    Body: buffer,
+    // Metadata: metadata,
+  }
+  const s3 = new S3Client({ region: process.env.BUCKET_REGION });
+
+  console.log(`Starting upload for ${key}`)
+  return s3.send(new PutObjectCommand(objectParams))
+    .then(res => {
+      console.log('Upload successful!', res);
+      return key;
+    })
+    .catch(err => {
+      console.warn("Failed to upload image:", err);
+      throw err;
+    })
+}
+
+/**
+ * @param {Object} event - Input event to the Lambda function
+ * @param {Object} context - Lambda Context runtime methods and attributes
+ *
+ * @returns {Object} object - Object containing feed item along with uploaded links
+ * 
+ */
+exports.uploader = async (event, context) => {
+  if (!event.image) {
+    console.warn("No image to upload")
+    return event;
+  }
+
+  return getImage(event.image)
+    .then(buffer => {
+      const image = sharp(buffer);
+      
+      return image.metadata().then(metadata => {
+        const meta = {
+          format: metadata.format,
+          width: metadata.width,
+          height: metadata.height,
+        }
+
+        return uploadImage(buffer, meta);
+      })
+    })
+    .then(key => {
+      return {
+        ...event,
+        s3ImageKey: key,
+      }
+    })
+    .catch(err => {
+      console.warn(err);
+      return event;
+    })
+};
