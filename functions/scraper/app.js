714eb7bc128ceac0325b1c80ae49ab2367c26f3b
Gagan Lokesh <gaganlokesh@gmail.com>
2022-01-11 22:39:00 +0530

    Add support for scraping sites which enforce blocking mechanisms for non-browser requests
    - Use `puppeteer` headless browser along with `browserless` driver to retrieve HTML document from any web page
    
    - Create new lambda layer and define dependencies for scraping in-order to reduce the overall package size of the scraper lambda function.
    
    - Add lambda layer definition to template.yml
    
    - Allocate 1024MB of memory for scraper function since chromium browser requires more memory to launch and run

--- functions/scraper/app.js
+++ functions/scraper/app.js
@@ -1,4 +1,5 @@
-const got = require('got');
+const createBrowserless = require('browserless');
+const chromium = require('chrome-aws-lambda');
 const metascraper = require('metascraper')([
   require('metascraper-author')(),
   require('metascraper-image')(),
@@ -11,7 +12,6 @@ const metascraper = require('metascraper')([
 const { Readability, isProbablyReaderable } = require('@mozilla/readability');
 const { JSDOM } = require('jsdom');
 const readingTime = require('reading-time');
-
 
 const getReadabilityMeta = (htmlString, url) => {
   const { document } = (new JSDOM(htmlString, { url })).window;
@@ -29,8 +29,38 @@ const getReadabilityMeta = (htmlString, url) => {
   return {};
 }
 
-const scrapeMetadata = async ({ url: targetUrl, content }) => {
-  const { body: html, url } = await got(targetUrl);
+const getHtml = async (url) => {
+  let browserlessFactory, browserless;
+  let html = null;
+
+  try {
+    const executablePath = await chromium.executablePath;
+    browserlessFactory = createBrowserless({
+      executablePath,
+      timeout: 30000,
+      headless: true,
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      ignoreHTTPSErrors: true,
+    });
+    browserless = await browserlessFactory.createContext();
+
+    html = await browserless.html(url);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    console.log("Destroying context and closing browser...");
+    if (browserless) await browserless.destroyContext();
+    if (browserlessFactory) await browserlessFactory.close();
+  }
+
+  return html;
+}
+
+const scrapeMetadata = async ({ url, content }) => {
+  const html = await getHtml(url);
+  if (!html) return {};
+
   const metadata = await metascraper({ html, url });
   const readabilityMeta = getReadabilityMeta(html, url);
   let articleContent = content || readabilityMeta?.content || null;
@@ -47,7 +77,7 @@ const scrapeMetadata = async ({ url: targetUrl, content }) => {
  * @param {Object} event - Input event to the Lambda function
  * @param {Object} context - Lambda Context runtime methods and attributes
  *
- * @returns {Object} object - Enriched feed item
+ * @returns {Object} object - Enriched feed item with metadata
  *
  */
 exports.scraper = async (event, context) => {
@@ -56,6 +86,6 @@ exports.scraper = async (event, context) => {
   return {
     ...event,
     ...metadata,
-    tags: event.tags?.length > 0 ? event.tags : (metadata.tags || []),
+    tags: event.tags?.length > 0 ? event.tags : (metadata?.tags || []),
   }
 };
